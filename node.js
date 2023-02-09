35756e2351755da3a3feea7ffb73a2e77fad17b7
Amuthan Arulraj <arulraj.amuthan@gmail.com>
2023-02-09 00:39:49 -0500

    Adding base question

---
+++ node.js
@@ -0,0 +1,64 @@
+/**
+* A Node represents an HTML Element. A node can have a tag name,
+* a list of CSS classes and a list of children nodes.
+*/
+class Node {
+
+  constructor(tag, children, classes) {
+    // Tag name of the node.
+    this.tag = tag;
+    // Array of CSS class names (string) on this element.
+    this.classes = classes;
+    // Array of child nodes.
+    this.children = children; // All children are of type Node
+  }
+  
+  /**
+  * Returns descendent nodes matching the selector. Selector can be 
+  * a tag name or a CSS class name.
+  * 
+  * For example: 
+  * 
+  * 1.
+  * <div> 
+  *   <span id="span-1"></span>
+  *   <span id="span-2"></span>
+  *   <div>
+  *     <span id="span-3"></span>
+  *   </div>
+  * </div>
+  * Selector `span` should return 3 span nodes in this order
+  * span-1 -> span-2 -> span-3.
+  *
+  * 2.
+  * <div> 
+  *   <span id="span-1" class="note"></span>
+  *   <span id="span-2"></span>
+  *   <div>
+  *     <span id="span-3"></span>
+  *   </div>
+  * </div>
+  * Selector `.note` should return one span node with `note` class.
+  *
+  * 3.
+  * <div> 
+  *   <span id="span-1" class="note></span>
+  *   <span id="span-2"></span>
+  *   <span id="span-3"></span>
+  *   <article>
+  *     <div>
+  *       <span id="span-3"></span>
+  *     </div>
+  *   </article>
+  * </div>
+  * Selector `.note ~ span` should return two span nodes.
+  * span-1 -> span-2.
+  * 
+  * @param {string} the selector string.
+  * @returns {Array} Array of descendent nodes.
+  * @public
+  */
+  search(selector) {
+  }
+
+}
